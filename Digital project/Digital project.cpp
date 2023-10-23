06dcdc41e1a02554e31ee477e01f33a06f012ea5
yussuf1235 <yussuf_mohamed2017@yahoo.com>
2023-10-23 14:34:07 +0300

    Test 2

--- Digital project/Digital project.cpp
+++ Digital project/Digital project.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Test part1 zoz\n";
+    std::cout << "Test part1 hmmmmmmmmmmmmmmmmmmmmmm\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
