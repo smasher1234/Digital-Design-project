04025d30a602fae38f88168b25779dbe704ef894
yussuf1235 <yussuf_mohamed2017@yahoo.com>
2023-10-25 16:20:36 +0300

    Added order by number of 1s
    Added some test values and 2 new functions
    one function gets the number of 1s in a binary string and the other compares two implicant to see which one has a higher number of ones

--- Project files/Implicant.h
+++ Project files/Implicant.h
@@ -6,13 +6,19 @@ using namespace std;
 
 class Implicant
 {
-
+private:
 	//this is a string which contains the boolean expression of the Implicant eg:01000,  -100-10, or 00-11
 	string Bool_expression;
 	//this is a vector which contains a list of the minterms the implicant consists of, eg: 18,32,50,4
 	vector<int> Indexes;
 
+
+public:
 	//constructor to set the starting Boolean expression and Index of the Implicant
 	Implicant(string& Bool_exp, int Starting_index);
+	//function that returns Bool_expression
+	string Get_Boolexp() { return Bool_expression; };
+	//function that returns value of Indexes vector at position index
+	int Get_Indexes(int index) { return Indexes[index]; };
 };
 
--- Project files/Project files.cpp
+++ Project files/Project files.cpp
@@ -1,26 +1,83 @@
-// Project files.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-using namespace std;
-//#include <Implicant.h>
+#include "Implicant.h"
+#include <algorithm>
 
-string convert_decimal_to_binary(int decimal, int &length);
+
+vector<int> minterms;
+//declare first implicant vector to be used, this vector will contains the resulted minterms, then it will be sorted and use in other functions
+vector<Implicant> imp1;
+
+
+//functions declaration
+string convert_decimal_to_binary(int decimal, int length);
+bool compareByOnesCount(Implicant& impli1, Implicant& impli2);
+int get_1_count(string binary);
+
 int main()
 {
-    //test random decimal and its value
-    int deci;
-    int size;
-    cin >> deci;
-    cin >> size;
-    cout << convert_decimal_to_binary(deci, size);
+    //test with random minterms and its value
+    minterms.push_back(12);
+    minterms.push_back(4);
+    minterms.push_back(7);
+    minterms.push_back(3);
+    minterms.push_back(5);
+    minterms.push_back(0);
+    minterms.push_back(14);
+    minterms.push_back(15);
+
+    //temp string to store decimal to binary result
+    string temp = "";
+    //loop over minterms
+    for (int x = 0; x < minterms.size(); x++) 
+    {
+        //convert the current minterm (decimal) to its binary representation
+        temp = convert_decimal_to_binary(minterms[x], 4);
+
+        //check if the conversion was succesful by checking if the string has the word "Error" or not
+        if (temp.find("Error") == string::npos) {
+
+            //create an implicant object and set its binary representation to temp which has been assigned before
+            Implicant implicant(temp, minterms[x]);
+
+            //add the result ob to the first implicants vector
+            imp1.push_back(implicant);
+        }
+    }
+    //use sort algorithm to order the implicant vector imp1 by the number of ones
+    sort(imp1.begin(), imp1.end(), compareByOnesCount);
+
+    //output results
+    for (int x = 0; x < imp1.size(); x++) {
+        cout << imp1[x].Get_Indexes(0) << "," << imp1[x].Get_Boolexp() << endl;
+    }
+}
+
+//a utility function that uses sort alogrithm
+bool compareByOnesCount(Implicant& impli1, Implicant& impli2) {
+    if (get_1_count(impli1.Get_Boolexp()) < get_1_count(impli2.Get_Boolexp())) 
+    {
+        return true;
+    }
+    return false;
+}
+
+//a utility function that count the number of 1s in a binary string
+int get_1_count(string binary) {
+
+    int counter=0;
+    for (int x = 0; x < binary.size(); x++) 
+    {     
+        if (binary[x] == '1') 
+        {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 
-
-
 //a utility function that converts a decimal (minterm) to a binary
-string convert_decimal_to_binary(int decimal, int& length)
+string convert_decimal_to_binary(int decimal, int length)
 {
     //check if the decimal is larger than the size
     if (decimal >= pow(2, length)) {
@@ -46,4 +103,31 @@ string convert_decimal_to_binary(int decimal, int& length)
     }
     //return desired binary representation string
     return binary_rep;
+}
+
+
+
+//Function to check if two minterms differ by only one bit
+bool the_difference_is_one_bit(int mint_1, int mint_2)
+{
+    int xor_result = mint_1 ^ mint_2; // XOR the two minterms to find differing bits
+    int count_of_ones = 0; // Initialize a count of differing bits
+    while (xor_result)
+    {
+        count_of_ones += xor_result & 1; // Count the number of differing bits
+        xor_result >>= 1; // Shift right to check the next bit
+    }
+    return count_of_ones == 1; // Return true if there is only one differing bit, indicating the minterms can be merged
+}
+// Function to merge two minterms into a new one
+int merge_the_minterms(int mint_1, int mint_2)
+{
+    int done_merged = 0; // Initialize the merged minterm
+    int xor_result = mint_1 ^ mint_2; // XOR the two minterms to find differing bits
+    while (xor_result)
+    {
+        done_merged = (done_merged << 1) | 1; // Shift left and add a 1 for each differing bit
+        xor_result >>= 1; // Shift right to check the next bit
+    }
+    return done_merged; // Return the merged minterm with differing bits combined
 }
